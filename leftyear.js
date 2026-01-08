80568b654f7945307c2b29915e93fdc30e6bae8e
coder-utshab <ghoshutshob470@gmail.com>
2026-01-09 00:51:51 +0530

    up

---
+++ leftyear.js
@@ -0,0 +1,47 @@
+
+//21-3 Check whether a year is a Leap Year or not
+
+function isLeapYear (year){
+    if(year % 4 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const isLipi = isLeapYear(2043);
+console.log(isLipi);
+
+
+
+
+
+// 1. those year thaat is not divisible by 100 and if the year is divisible by 
+// 4: then it will be a leap year
+// * 2. if the year is divisible by 400 then it is a leap year
+// * 3. else it is not a leap year
+
+
+
+function isLeapYear2(year){
+    if (year % 400 === 0) {
+        return true;
+    }
+    else if (year % 100 === 0) {
+        return false;
+    }
+    else if (year % 4 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const isLeap = isLeapYear2(2100);
+const isLeap2 = isLeapYear2(2400);
+const isLeap3 = isLeapYear2(1900);
+const isLeap4 = isLeapYear2(2052);
+
+console.log(isLeap, isLeap2, isLeap3, isLeap4);
