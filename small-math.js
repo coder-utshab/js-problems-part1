a9f325ad554d3d2eb0b498b1fe276a8e5948ec19
coder-utshab <ghoshutshob470@gmail.com>
2026-01-09 21:47:19 +0530

    js

---
+++ small-math.js
@@ -0,0 +1,16 @@
+
+// 21-6 Math, abs, round, ceil, floor, and random number
+
+
+const min = Math.min(45, 56, 12, 13, 28,)
+console.log(min);
+
+
+//MAX NUMBER BAR KORAR JONNO
+const max = Math.max(52, 86, 41, 12, 24, 86, -12 )
+console.log(max);
+console.log(Math.PI);
+console.log(Math.floor(2.95))
+console.log(Math.random()*10)
+const rand = Math.round(Math.random()*10)
+console.log(rand)
