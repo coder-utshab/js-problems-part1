9e42386129258a64999ccf2c04d0df757257705b
coder-utshab <ghoshutshob470@gmail.com>
2026-01-10 21:19:42 +0530

    up

---
+++ max2.js
@@ -0,0 +1,13 @@
+const jim = 256;
+const tim = 389;
+const kim = 168;
+
+if(jim > tim && jim > kim) {
+    console.log('jim is the all time boss')
+}
+else if (tim > jim && tim > kim){
+    console.log('tim is the boss')
+}
+else{
+    console.log('kim is the kardashians boss')
+}
