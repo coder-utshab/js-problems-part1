0340c241db244ae9dd0f77f968361017eae54504
coder-utshab <ghoshutshob470@gmail.com>
2026-01-11 11:51:49 +0530

    js

--- layerd.js
+++ layerd.js
@@ -1,1 +1,38 @@
 //22-6 (advanced) Multi-layer discount price calculation
+//first100 --> 100
+//101 To 200 --> 90
+//above 200 --> 70
+
+
+// function layerdDiscountedTotal (quantity){
+function layeredDiscountedTotal(quantity){
+    const first100Price = 100;
+    const second100Price = 90;
+    const above200Price = 70;
+
+
+    if(quantity <= 100){
+        const total = quantity * first100Price;
+        return total;
+    }
+    else if (quantity <= 200){
+       const first100Total = 100 * first100Price;
+       const remainingQuantity = quantity - 100;
+       const remainingTotal = remainingQuantity * second100Price;
+       const total = first100Total + remainingTotal;
+       return total;
+    }
+    else{
+        const first100Total = 100 * first100Price;
+        // const secoend100Totaal = 100 * second100Price;
+        const second100Total = 100 * second100Price;
+        const remainingQuantity = quantity - 200;
+        const remainingTotal = remainingQuantity * above200Price;
+        const total = first100Total + second100Total + remainingTotal;
+        return total;
+    }
+}
+
+console.log(layeredDiscountedTotal(50));
+console.log(layeredDiscountedTotal(150));
+console.log(layeredDiscountedTotal(250));
