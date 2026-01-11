b27cc1904a469ede8bff5b610ec05476b5044abe
coder-utshab <ghoshutshob470@gmail.com>
2026-01-11 17:02:26 +0530

    up

---
+++ object.js
@@ -0,0 +1,13 @@
+const student = {
+    name: 'utshab kumar ghosh',
+    id: 2403051051388,
+    address: 'gujrat',
+    isSingle: true,
+    friends: ['sajib', 'animesh', 'shuvo', 'avijeet','dipto'],
+    car:{
+        brand:'tesla',
+        price: 50000000,
+        made: 2024,
+    }
+}
+console.log(student.car);
